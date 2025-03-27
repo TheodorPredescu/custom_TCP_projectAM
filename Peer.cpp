964ef42e2928563215a125e66002ce63b4fa4d5f
TheodorPredescu <theodorpredescu@gmail.com>
2025-03-27 11:33:25 +0200

    Added thread-ing in peer
    (need to check it better)

--- Peer.cpp
+++ Peer.cpp
@@ -21,12 +21,19 @@ public:
 
 private:
   int sock;
+
+  std::mutex packet_mutex; // Mutex for thread-safe access to the packet queue
+  std::queue<CustomPacket> packet_queue; // Queue to store received packets
+  std::condition_variable packet_cv; // Condition variable for packet processing
+
   sockaddr_in peer_addr;
   u_int16_t id_packet = 0;
+
   void sendPacket(CustomPacket &packet);
-  void receivePacket();
+  void receivePacket(CustomPacket &packet);
   void listenForPackets();
   void connectToPeer(const char *remote_ip);
+  void processPackets();
 
   // for later
   void composePacketMessage();
@@ -40,28 +47,14 @@ private:
 // gets a package and sends it to the socket sock
 // working to make it decent with bigger package
 void Peer::sendPacket(CustomPacket &packet) {
-
   uint8_t buffer[sizeof(CustomPacket)];
-
-  // // The packet_id and checksum will be added here; cannot fit verry well in
-  // the
-  // // CustomPacket struct
-  // if (id_packet >= UINT16_MAX - 1) {
-  //   std::cerr << "Warning: Packet ID overflow. Resetting...\n";
-  //   id_packet = 0;
-  // }
-  //
-  // packet.packet_id = id_packet;
-  // packet.checksum = packet.calculateChecksum(packet);
-
   packet.serialize(buffer);
   send(sock, buffer, sizeof(buffer), 0);
-  std::cout << "Packet sended to: " << sock;
+  std::cout << "Packet sent to: " << sock << "\n";
 }
 
 // TODO check everithing
 void Peer::startPeer(int port, const char *remote_ip) {
-
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     std::cerr << "Error creating socket!\n";
     return;
@@ -83,8 +76,7 @@ void Peer::startPeer(int port, const char *remote_ip) {
     peer_addr.sin_addr.s_addr = INADDR_ANY;
 
     if (bind(sock, (struct sockaddr *)&peer_addr, sizeof(peer_addr)) < 0) {
-
-      std::cerr << "Error: Failer to bind socket to port" << port << std::endl;
+      std::cerr << "Error: Failed to bind socket to port " << port << std::endl;
       close(sock);
       return;
     }
@@ -92,14 +84,19 @@ void Peer::startPeer(int port, const char *remote_ip) {
     listen(sock, 3);
   }
 
+  // Start threads for receiving and processing packets
   std::thread receiver(&Peer::listenForPackets, this);
-  receiver.detach();
+  std::thread processor(&Peer::processPackets, this);
 
+  receiver.detach();
+  processor.detach();
+
+  // Example: Send an initial packet
   CustomPacket packet;
   packet.packet_id = 1;
   strcpy(packet.payload, "Hello from peer starter!");
   packet.length = strlen(packet.payload);
-  packet.checksum = packet.calculateChecksum(packet);
+  packet.checksum = packet.calculateChecksum();
 
   sendPacket(packet);
 }
@@ -112,18 +109,16 @@ void Peer::startPeer(int port, const char *remote_ip) {
 // success" message !! OOOOR I  SEND FIRST A PACKET THAT TELLS ME THE NUMBER OF
 // PACKAGES THAT WILL BE SEND AND I JUST CHECK IF THAT NUMBER OF PACKAGES ARE
 // MET
-void Peer::receivePacket() {
-  u_int8_t buffer[sizeof(CustomPacket)];
+void Peer::receivePacket(CustomPacket &packet) {
+  uint8_t buffer[sizeof(CustomPacket)];
   int valread = read(sock, buffer, sizeof(CustomPacket));
 
   if (valread > 0) {
-
-    CustomPacket packet = CustomPacket::deserialize(buffer);
+    packet = CustomPacket::deserialize(buffer);
     std::cout << "Received Packet ID: " << packet.packet_id << "\n";
     std::cout << "Payload: " << packet.payload << "\n";
-
   } else {
-    std::cout << "Error reading packet\n";
+    std::cerr << "Error reading packet\n";
   }
 }
 
@@ -142,6 +137,55 @@ void Peer::listenForPackets() {
   }
 
   while (true) {
-    receivePacket();
+    CustomPacket packet;
+    receivePacket(packet);
+
+    // Add the packet to the queue
+    {
+      std::lock_guard<std::mutex> lock(packet_mutex);
+      packet_queue.push(packet);
+    }
+    packet_cv.notify_one(); // Notify the processing thread
   }
+}
+
+void Peer::processPackets() {
+  while (true) {
+    CustomPacket packet;
+
+    // Wait for a packet to be available
+    {
+      std::unique_lock<std::mutex> lock(packet_mutex);
+      packet_cv.wait(lock, [this] { return !packet_queue.empty(); });
+
+      // Get the next packet from the queue
+      packet = packet_queue.front();
+      packet_queue.pop();
+    }
+
+    // Validate the packet
+    if (packet.calculateChecksum() != packet.checksum) {
+      std::cerr << "Packet with ID " << packet.packet_id << " is corrupted!\n";
+      continue;
+    }
+
+    // Process the packet (e.g., add to a map, reconstruct a message, etc.)
+    std::cout << "Processing Packet ID: " << packet.packet_id << "\n";
+    std::cout << "Payload: " << packet.payload << "\n";
+  }
+}
+
+int main() {
+  Peer peer;
+  peer.startPeer(8080); // Start the peer on port 8080
+
+  // Simulate sending a message
+  peer.sendMessage("Hello, this is a test message!");
+
+  // Keep the main thread alive
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+
+  return 0;
 }
